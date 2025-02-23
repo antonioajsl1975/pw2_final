1c0ec7bef1be0a5f12db50dba30701c8a1d9a5a4
antonioajsl1975 <antonioajsl1975@gmail.com>
2025-02-23 19:24:52 -0300

    Validação de departamento implementada

---
+++ src/main/java/br/edu/ifto/aula09/controller/LoginController.java
@@ -0,0 +1,15 @@
+package br.edu.ifto.aula09.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/login")
+public class LoginController {
+
+    @PostMapping("/login")
+    public String loginPost() {
+        return "login";
+    }
+}
